826489d8cd6db51dd4ca4be798752e3b0e9dcde6
MailingDelgadoMedina <40926855+MailingDelgadoMedina@users.noreply.github.com>
2023-06-18 03:16:51 +0000

    Added first example

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-#  first commit
-print("Hello World")
+#  FOR LOOPS
+
+# Sequence- ordering of expresions in your program
+# Selection - making choices like if/else
+# Iteration - loops
+# All programing language have the 3 main control flow and with those you can write any algorithm.
+
+#Example 1
+for i in range(10):
+  print("Hello World")

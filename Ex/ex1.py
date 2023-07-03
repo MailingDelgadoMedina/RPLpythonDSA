93b7df360cfd8f28a02dd765e1757213aee3e4d0
MailingDelgadoMedina <40926855+MailingDelgadoMedina@users.noreply.github.com>
2023-07-03 01:56:42 +0000

    Finished with the loops on ex1.py

---
+++ Ex/ex1.py
@@ -0,0 +1,65 @@
+"""
+1. Write a program that prints your name 100 times.
+2. Write a program to fill the screen horizontally and vertically with your name. [Hint: add the
+option end='' into the print function to fill the screen horizontally.]
+3. Write a program that outputs 100 lines, numbered 1 to 100, each with your name on it. The
+output should look like the output below.
+
+"""
+
+"""
+for i in range(100):
+  print(i+ 1,'Mailing Delgado')
+"""
+"""
+for i in range(1, 21):
+    square = i ** 2
+    print(f"{i} --- {square}")
+"""
+"""
+# for num in range(8, 90, 3):
+#     print(num)
+# for numero in range(100, 0, -2):
+#   print(numero)
+
+# for a in range(10):
+#   print("A", end = " ")
+# for b in range(7):
+#   print("B", end = ' ')
+# for cd in range(4):
+#   print("CD", end = ' ')
+# print("E", end = ' ')
+# for f in range(6):
+#   print("F", end = ' ')
+# print("G", end = ' ' )
+"""
+
+"""
+min = eval(input("Enter a minum value: "))
+max = eval(input("Enter a max value: "))
+inc = eval(input("Enter the increment value: "))
+print("*" *20)
+print("{:>5}{:>10}".format("Value","Squared"))
+print("-"*15)
+for i in range(min ,max, inc):
+  print("{:>5}{:>10}".format(i, i*i))
+
+"""
+"""
+for name in "Mei":
+  print(name, end=" ")
+"""
+
+
+# for my_list in ["Anina", "Bruni","Mario","Mei"]:
+#   print(my_list)
+
+"""
+quant_num = eval(input("Enter how many numbers: "))
+total = 0
+for i in range(quant_num):
+  num = eval(input("Enter a number: "))
+  total = total + num
+print("The average is {:.2f".format(total/quant_num) )
+
+"""
